9102ea447669230d9889449cc01e5a0da6c39c15
FsDk <cayuela@gmail.com>
2022-04-16 19:41:09 +0200

    se agrega 1 constante con los egresos o reintegros

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,8 @@ const ingresos = [
     new Ingreso('Sueldo', 2.000),
     new Ingreso('Venta de Palet de Estrella 1L', 800)
 ];
+
+const egresos = [
+    new Egreso('Renta', 900),
+    new Egreso('Atun', 2.50)
+];
